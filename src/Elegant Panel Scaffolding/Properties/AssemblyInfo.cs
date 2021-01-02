cfe387bc1176e990af60f7e786af30972e3ea415
ProfessorAire <22963406+ProfessorAire@users.noreply.github.com>
2021-01-02 10:06:22 -0800

    Updated version information.

--- src/Elegant Panel Scaffolding/Properties/AssemblyInfo.cs
+++ src/Elegant Panel Scaffolding/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
